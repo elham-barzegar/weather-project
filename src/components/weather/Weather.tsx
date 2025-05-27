787bcf32d91b638a4fdb6ee4af3a39deeb80544c
nano byte <elhambarzegar1403@gmail.com>
2025-05-28 01:02:20 +0330

    Implement Weather Component

--- src/components/weather/Weather.tsx
+++ src/components/weather/Weather.tsx
@@ -1,12 +1,27 @@
 import {SearchForm} from "@/components/weather/SearchForm";
 import {WeatherInfo} from "@/components/weather/WeatherInfo";
 import {ForecastList} from "@/components/weather/ForecastList";
+import {useState} from "react";
 
 interface props{
-
+    city: string,
 }
-export function Weather ({}:props){
-    return(
+export function Weather ({city}:props){
+
+    const [weatherState, setWeatherState] = useState<Weather>({
+        city: city,
+        wind: '',
+        humidity:'',
+        description: '',
+        icon: '',
+        daily: []
+    })
+
+   // const callWeatherApi = (()=>{
+   //
+   //  })
+
+    return (
         <>
 
             <SearchForm/>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <main>
 
-      <Weather/>
+      <Weather city={"tehran"}/>
 
     </main>
   );
