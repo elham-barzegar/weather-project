fd2b9001bf12aa53c2fcdd06693d5ef1f7993982
nano byte <elhambarzegar1403@gmail.com>
2025-05-30 17:49:20 +0330

    Implement Weather api & Search Form

--- src/components/weather/SearchForm.tsx
+++ src/components/weather/SearchForm.tsx
@@ -1,10 +1,25 @@
+import {ChangeEvent, FormEvent, FormEventHandler, useState} from "react";
+
 interface Props {
-
+    city:string,
+    getWeatherData: Function,
 }
-export function SearchForm({}: Props) {
-    return (
-        <div>
+export function SearchForm({city,getWeatherData}: Props) {
+    const [nameState, setNameState] = useState<string>(city)
 
-        </div>
+    const cityNameChangeHandler = (e: ChangeEvent<HTMLInputElement>)=>{
+        setNameState(e.target.value)
+    }
+    const submitHandler =(e: FormEvent<HTMLFormElement>)=>{
+        e.preventDefault()
+        getWeatherData(nameState)
+    }
+
+    return (
+        <form onSubmit={submitHandler}>
+            <input type={"text"} name={"cityName"} className={"border rounded p-3"} onChange={cityNameChangeHandler} value={nameState}/>
+            <input type={"submit"} className={"bg-primary px-6 py-3 ml-3 text-white rounded"} value={"search"}/>
+
+        </form>
     );
 }
